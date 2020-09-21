fa22c8c61f781ea2b768d6a0692c88da197c0e7a
Akash <aakash@hey.com>
2020-09-21 18:51:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+I found myself doing this repeatedly so decided to create a base template which I can just mirror.
+
+- React
+- Typescript
+- TailwindCSS
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-5xl text-center font-semibold tracking-wide uppercase">
+        hello
+      </h1>
+    </div>
+  );
+}
+
+export default App;
