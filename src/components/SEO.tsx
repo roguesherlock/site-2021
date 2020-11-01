99834f992b75531874a182d0c953f63897a40897
Akash <aakash@hey.com>
2020-11-01 15:11:12 +0800

    fix seo

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -18,22 +18,20 @@ export default function SEO({ meta }: PropsWithChildren<SEOProps>) {
   const image = `${config.siteUrl}/${meta.image ?? config.siteImage}`;
   return (
     <Head>
-      <Head>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={config.username} />
-        <meta name="twitter:creator" content={config.username} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={meta.spoiler} />
-        <meta name="twitter:image" content={image} />
-        <meta
-          property="og:url"
-          content={`${config.siteUrl}/${router.pathname}`}
-        />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={meta.spoiler} />
-        <meta property="og:image" content={image} />
-      </Head>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={config.username} />
+      <meta name="twitter:creator" content={config.username} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={meta.spoiler} />
+      <meta name="twitter:image" content={image} />
+      <meta
+        property="og:url"
+        content={`${config.siteUrl}/${router.pathname}`}
+      />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={meta.spoiler} />
+      <meta property="og:image" content={image} />
     </Head>
   );
 }
